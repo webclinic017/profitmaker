1c851813e87dab197ca19584072ed0d8cb6afc77
Eugen Soloviov <suenot@gmail.com>
2025-06-07 23:04:43 +0300

    clear for new version

--- packages/express-server/core_components/getCCXTId.js
+++
@@ -1,9 +0,0 @@
-
-const getCCXTId = function(accountId, type) {
-  try {
-    return global.ACCOUNTS[accountId][type]
-  } catch (err) {
-    console.log(err)
-  }
-}
-exports.getCCXTId = getCCXTId
--- packages/express-server/utils/authMiddleware/authMiddleware.js
+++
@@ -1,9 +0,0 @@
-const authMiddleware = (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    res.status(401).send('You are not authenticated')
-  } else {
-    return next()
-  }
-}
-
-exports.authMiddleware = authMiddleware
